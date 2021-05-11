61b01435daa7d208c3fb775ce53fd64510752590
Berthelemy Clotilde <berthele@insa-20708>
2021-05-11 18:18:14 +0200

    implementation de l'algo A*

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -1,9 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
 
+import org.insa.graphs.model.Point;
+
 public class AStarAlgorithm extends DijkstraAlgorithm {
 
     public AStarAlgorithm(ShortestPathData data) {
         super(data);
     }
-
+    
+    //Redefinition of setLabel()
+    public void setLabel(ShortestPathData data) {
+    	for (int i=0; i<data.getGraph().size(); i++) {
+    		label[i]=new LabelStar(i,false,Double.POSITIVE_INFINITY,-1,Point.distance(data.getGraph().getNodes().get(i).getPoint(), data.getDestination().getPoint()));
+    	}
+    }
 }
